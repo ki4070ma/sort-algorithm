39ae9949887b7a8875c3d321339d20d9c9e860b4
Atsushi Mori <ki4070ma@gmail.com>
2019-03-21 15:25:48 +0900

    Added selection sort

--- sort.py
+++ sort.py
@@ -1,7 +1,23 @@
 
+def selection_sort(l):
+    length = len(l)
+    for i in range(length - 1, -1, -1):  # TODO Hard to understand
+        idx_min = length - 1 - i
+        for j in range(length - i, length):
+            if l[idx_min] > l[j]:
+                idx_min = j
+        min_val = l[idx_min]
+        l[idx_min] = l[length - i - 1]
+        l[length - i - 1] = min_val
+        print('min: %d' % min_val)
+        print(l)
+    return l
+
+
+
 def bubble_sort(l):
     length = len(l)
-    for i in range(length - 1, -1, -1):
+    for i in range(length - 1, -1, -1):  # TODO Hard to understand
         for j in range(length - 1, length - i - 1 , -1):
             # print(l[j], l[j - 1])
             print(l)
@@ -16,6 +32,7 @@ def bubble_sort(l):
 if __name__ == '__main__':
     input_list = [5, 9, 3, 1, 2, 8, 4, 7, 6]
 
-    sorted_list = bubble_sort(input_list)
+    # sorted_list = bubble_sort(input_list)
+    sorted_list = selection_sort(input_list)
 
     assert sorted_list == [1, 2, 3, 4, 5, 6, 7, 8, 9]
